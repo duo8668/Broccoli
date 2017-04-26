390050f9763130d5077f87a758fcaa4a9592624d
duo8668 <duomax8668@hotmail.com>
2017-04-26 22:32:24 +0800

    Build Model
    Build Model for Database query

---
+++ Broccoli.Core/Database/Attributes/SqlTableNameAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Broccoli.Core.Database.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class SqlTableNameAttribute : Attribute
+    {
+        public readonly string Value;
+
+        public SqlTableNameAttribute(string value)
+        {
+            this.Value = value;
+        }
+    }
+}
---
+++ Broccoli.Core/Database/Eloquent/Model.cs
@@ -0,0 +1,73 @@
+﻿using Broccoli.Core.Database.Attributes;
+using Inflector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Broccoli.Core.Database.Eloquent
+{
+    public class Model<TModel>
+    {
+        protected PetaPoco.Database dbCtx;
+
+        protected string primaryKey { get; set; }
+
+        private string _SqlTableName;
+
+        protected virtual string SqlTableName
+        {
+            get
+            {
+                if (_SqlTableName == null)
+                {
+                    try
+                    {
+                        // First lets see if the model has it's own table name.
+                        _SqlTableName = this.GetType()
+                           .GetCustomAttribute<SqlTableNameAttribute>(false)
+                           .Value;
+                    }
+                    catch (NullReferenceException)
+                    {
+                        // Calculate the table name based on the class name.
+                        var typeString = this.GetType().ToString();
+                        var typeParts = typeString.Split('.');
+                        _SqlTableName = typeParts[typeParts.Length - 1];
+                        _SqlTableName = _SqlTableName.Pluralize();
+                    }
+                }
+
+                return _SqlTableName;
+            }
+        }
+
+        public TModel Find(string primaryKey, bool withTrashed = false)
+        {
+            var _sql = PetaPoco.Sql.Builder
+                .Select("*")
+                .Where("id = @0", primaryKey);
+            if (!withTrashed)
+            {
+                _sql = _sql.Where("date_created IS NOT NULL");
+            }
+
+            return dbCtx.First<TModel>(_sql);
+        }
+
+        [DataType(DataType.Text)]
+        public string RecordInfo { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedAt { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime ModifiedAt { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? DeletedAt { get; set; }
+    }
+}
